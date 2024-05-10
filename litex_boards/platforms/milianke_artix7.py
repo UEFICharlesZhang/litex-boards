bfdf1f9f959d4b8bc64261f0f2d7e5aad919f27f
Your Name <you@example.com>
2024-05-10 16:56:47 +0800

    add support for milianke_artix7 100t board

---
+++ litex_boards/platforms/milianke_artix7.py
@@ -0,0 +1,180 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import Xilinx7SeriesPlatform, VivadoProgrammer
+from litex.build.openocd import OpenOCD
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk / Rst
+    ("clk50", 0, Pins("V4"), IOStandard("SSTL15")),
+    ("cpu_reset", 0, Pins("R14"), IOStandard("LVCMOS33")), # KEY1
+
+    # Leds
+    ("user_led",  0, Pins("E21"), IOStandard("LVCMOS33")),
+    ("user_led",  1, Pins("D21"), IOStandard("LVCMOS33")),
+    ("user_led",  2, Pins("E22"), IOStandard("LVCMOS33")),
+    ("user_led",  3, Pins("D22"), IOStandard("LVCMOS33")),
+
+    # Buttons
+    # ("user_btn", 0, Pins("R14"), IOStandard("LVCMOS33")), #KEY1
+    ("user_btn", 1, Pins("P14"), IOStandard("LVCMOS33")),#KEY2
+    ("user_btn", 2, Pins("N14"), IOStandard("LVCMOS33")),#KEY3
+    ("user_btn", 3, Pins("N13"), IOStandard("LVCMOS33")),#KEY4
+    # ("user_btn", 4, Pins("M18"), IOStandard("LVCMOS33")),
+
+    # Serial
+    ("serial", 0,
+        Subsignal("tx", Pins("R17")),
+        Subsignal("rx", Pins("P16")),
+        IOStandard("LVCMOS33"),
+    ),
+
+    # SPIFlash
+    ("spiflash", 0,
+        Subsignal("cs_n", Pins("T19")),
+        Subsignal("mosi", Pins("P22")),
+        Subsignal("miso", Pins("R22")),
+        Subsignal("vvp",  Pins("P21")),
+        Subsignal("hold", Pins("R21")),
+        IOStandard("LVCMOS33")
+    ),
+    ("spiflash4x", 0,  # clock needs to be accessed through STARTUPE2
+        Subsignal("cs_n", Pins("T19")),
+        Subsignal("dq",   Pins("P22", "R22", "P21", "R21")),
+        IOStandard("LVCMOS33")
+    ),
+    # SDCard
+    ("spisdcard", 0,
+        Subsignal("clk",  Pins("W17")),
+        Subsignal("mosi", Pins("AA19"), Misc("PULLUP True")),
+        Subsignal("cs_n", Pins("AB12"), Misc("PULLUP True")),
+        Subsignal("miso", Pins("V17"), Misc("PULLUP True")),
+        Misc("SLEW=FAST"),
+        IOStandard("LVCMOS33"),
+    ),
+    ("sdcard", 0,
+        Subsignal("data", Pins("V17 AB18 Y19 AB12"), Misc("PULLUP True")),
+        Subsignal("cmd",  Pins("AA19"),              Misc("PULLUP True")),
+        Subsignal("clk",  Pins("W17")),
+        Subsignal("cd",   Pins("AA18")),
+        Misc("SLEW=FAST"),
+        IOStandard("LVCMOS33"),
+    ),
+
+
+
+    # DDR3 SDRAM
+    # MT41K128M16JT-125K
+    ("ddram", 0,
+        Subsignal("a", Pins("AB3 AA6 Y3 Y2 AB6 AA3 Y7 AA4",
+                            "AA8 Y4 Y9 AB7 AA5 W5 AB8"),
+            IOStandard("SSTL15")),
+        Subsignal("ba",    Pins("AB2 AB5 W2"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("V2"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("AA1"), IOStandard("SSTL15")),
+        Subsignal("we_n",  Pins("W1"), IOStandard("SSTL15")),
+        # cs_n is hardwired on the board
+        Subsignal("cs_n",  Pins("Y1"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("P2 J6 D2 H2"), IOStandard("SSTL15")),
+        Subsignal("dq", Pins(
+            "P6 R1 M5 N4 N5 N2 M6 P1",
+            "L3 J4 M3 K4 M2 K3 L4 L5",
+            "B1 E2 B2 F3 A1 G1 C2 F1",
+            "J5 H5 K1 G4 H4 G3 H3 G2"
+            ),
+            IOStandard("SSTL15"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("dqs_p", Pins("P5 M1 E1 K2"),
+            IOStandard("DIFF_SSTL15"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("dqs_n", Pins("P4 L1 D1 J2"),
+            IOStandard("DIFF_SSTL15"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("clk_p", Pins("T5"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("U5"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke",   Pins("Y6"), IOStandard("SSTL15")),
+        Subsignal("odt",   Pins("AB1"), IOStandard("SSTL15")),
+        Subsignal("reset_n", Pins("W4"), IOStandard("SSTL15")),
+        Misc("SLEW=FAST"),
+    ),
+    # RGMII Ethernet (RTL8211FD)
+    ("eth_clocks", 0,
+        Subsignal("tx", Pins("AB21")),
+        Subsignal("rx", Pins("W19")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 0,
+        Subsignal("rst_n",   Pins("U17")),
+        Subsignal("mdio",    Pins("N17")),
+        Subsignal("mdc",     Pins("U18")),
+        Subsignal("rx_ctl",  Pins("V18")),
+        Subsignal("rx_data", Pins("V19 W20 AA20 AA21")),
+        Subsignal("tx_ctl",  Pins("AB22")),
+        Subsignal("tx_data", Pins("W21 W22 Y21 Y22")),
+        IOStandard("LVCMOS33")
+    ),
+]
+
+# Connectors ---------------------------------------------------------------------------------------
+
+# _connectors = [
+#     ("pmoda", "C17 D18 E18 G17 D17 E17 F18 G18"),
+#     ("pmodb", "D14 F16 G16 H14 E16 F13 G13 H16"),
+#     ("pmodc", "K1 F6 J2 G6 E7 J3 J4 E6"),
+#     ("pmodd", "H4 H1 G1 G3 H2 G4 G2 F3"),
+#     ("pmodxdac", "A13 A15 B16 B18 A14 A16 B17 A18"),
+# ]
+_connectors = []
+# PMODS --------------------------------------------------------------------------------------------
+
+# def sdcard_pmod_io(pmod):
+#     return [
+#         # SDCard PMOD:
+#         # - https://store.digilentinc.com/pmod-microsd-microsd-card-slot/
+#         ("spisdcard", 0,
+#             Subsignal("clk",  Pins(f"{pmod}:3")),
+#             Subsignal("mosi", Pins(f"{pmod}:1"), Misc("PULLUP True")),
+#             Subsignal("cs_n", Pins(f"{pmod}:0"), Misc("PULLUP True")),
+#             Subsignal("miso", Pins(f"{pmod}:2"), Misc("PULLUP True")),
+#             Misc("SLEW=FAST"),
+#             IOStandard("LVCMOS33"),
+#         ),
+#         ("sdcard", 0,
+#             Subsignal("data", Pins(f"{pmod}:2 {pmod}:4 {pmod}:5 {pmod}:0"), Misc("PULLUP True")),
+#             Subsignal("cmd",  Pins(f"{pmod}:1"), Misc("PULLUP True")),
+#             Subsignal("clk",  Pins(f"{pmod}:3")),
+#             Subsignal("cd",   Pins(f"{pmod}:6")),
+#             Misc("SLEW=FAST"),
+#             IOStandard("LVCMOS33"),
+#         ),
+# ]
+# _sdcard_pmod_io = sdcard_pmod_io("pmodd") # SDCARD PMOD on JD.
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(Xilinx7SeriesPlatform):
+    default_clk_name   = "clk50"
+    default_clk_period = 1e9/50e6
+
+    def __init__(self, toolchain="vivado"):
+        Xilinx7SeriesPlatform.__init__(self, "xc7a100t-2fgg484", _io, _connectors, toolchain=toolchain)
+        self.add_platform_command("set_property INTERNAL_VREF 0.900 [get_iobanks 34]")
+        self.toolchain.bitstream_commands = \
+            ["set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]"]
+        self.toolchain.additional_commands = \
+            ["write_cfgmem -force -format mcs -interface spix4 -size 16 "
+            "-loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.mcs"]
+    def create_programmer(self):
+        return OpenOCD("openocd_xc7_ft232.cfg", "bscan_spi_xc7a100t.bit")
+
+    def do_finalize(self, fragment):
+        Xilinx7SeriesPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk50",             loose=True), 1e9/50e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:rx", 0, loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:rx", 1, loose=True), 1e9/125e6)
---
+++ litex_boards/targets/milianke_artix7.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from migen import *
+
+from litex.gen import *
+
+from litex_boards.platforms import milianke_artix7
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+from litedram.modules import MT41K128M16
+from litedram.phy import s7ddrphy
+from liteeth.phy.s7rgmii import LiteEthPHYRGMII
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst          = Signal()
+        self.cd_sys       = ClockDomain()
+        self.cd_sys4x     = ClockDomain()
+        self.cd_sys4x_dqs = ClockDomain()
+        self.cd_idelay    = ClockDomain()
+        # self.cd_eth       = ClockDomain()
+        # self.cd_vga       = ClockDomain()
+        # # #
+
+        self.pll = pll = S7MMCM(speedgrade=-2)
+        self.comb += pll.reset.eq(~platform.request("cpu_reset") | self.rst)
+        pll.register_clkin(platform.request("clk50"), 50e6)
+        pll.create_clkout(self.cd_sys,       sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
+        pll.create_clkout(self.cd_idelay,    200e6)
+        # pll.create_clkout(self.cd_eth,       50e6)
+        # pll.create_clkout(self.cd_vga,       40e6)
+        platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin) # Ignore sys_clk to pll.clkin path created by SoC's rst.
+
+        self.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=100e6,
+        with_ethernet          = False,
+        with_etherbone         = False,
+        eth_phy          = 0,
+        eth_ip           = "192.168.31.50",
+        eth_dynamic_ip  = False,
+        with_led_chaser        = True,
+        with_spi_flash         = False,
+        # with_video_terminal    = False,
+        # with_video_framebuffer = False,
+        **kwargs):
+        platform = milianke_artix7.Platform()
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq)
+
+        # SoCCore ----------------------------------_-----------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on Milianke", **kwargs)
+
+        # DDR3 SDRAM -------------------------------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            self.ddrphy = s7ddrphy.A7DDRPHY(platform.request("ddram"),
+                memtype        = "DDR3",
+                nphases        = 4,
+                sys_clk_freq   = sys_clk_freq)
+            self.add_sdram("sdram",
+                phy           = self.ddrphy,
+                module        = MT41K128M16(sys_clk_freq, "1:4"),
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+
+        # Ethernet / Etherbone ---------------------------------------------------------------------
+        if with_ethernet or with_etherbone:
+            self.ethphy = LiteEthPHYRGMII(
+                clock_pads = self.platform.request("eth_clocks", eth_phy),
+                pads       = self.platform.request("eth", eth_phy),
+                tx_delay   = 0e-9)
+            if with_ethernet:
+                self.add_ethernet(phy=self.ethphy, dynamic_ip=eth_dynamic_ip, data_width=32)
+            if with_etherbone:
+                self.add_etherbone(phy=self.ethphy, ip_address=eth_ip, data_width=32)
+
+        # # Video ------------------------------------------------------------------------------------
+        # if with_video_terminal or with_video_framebuffer:
+        #     self.videophy = VideoVGAPHY(platform.request("vga"), clock_domain="vga")
+        #     if with_video_terminal:
+        #         self.add_video_terminal(phy=self.videophy, timings="800x600@60Hz", clock_domain="vga")
+        #     if with_video_framebuffer:
+        #         self.add_video_framebuffer(phy=self.videophy, timings="800x600@60Hz", clock_domain="vga")
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            self.leds = LedChaser(
+                pads         = platform.request_all("user_led"),
+                sys_clk_freq = sys_clk_freq)
+        # SPI Flash --------------------------------------------------------------------------------
+        if with_spi_flash:
+            from litespi.modules import N25Q128A13
+            from litespi.opcodes import SpiNorFlashOpCodes as Codes
+            self.add_spi_flash(mode="4x", module=N25Q128A13(Codes.READ_1_1_4), rate="1:1", with_master=True)
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=milianke_artix7.Platform, description="LiteX SoC on Milianke.")
+    parser.add_target_argument("--sys-clk-freq", default=100e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--with-spi-flash",  action="store_true",       help="Enable SPI Flash (MMAPed).")
+
+    ethopts = parser.target_group.add_mutually_exclusive_group()
+    ethopts.add_argument("--with-ethernet",         action="store_true", help="Enable Ethernet support.")
+    ethopts.add_argument("--with-etherbone",        action="store_true", help="Enable Etherbone support.")
+    parser.add_target_argument("--eth-phy",           default=0, type=int,    help="Ethernet PHY (0 or 1).")
+    parser.add_target_argument("--eth-ip",            default="192.168.31.50", help="Ethernet/Etherbone IP address.")
+    parser.add_target_argument("--eth-dynamic-ip",    action="store_true",       help="Enable dynamic Ethernet IP addresses setting.")
+    sdopts = parser.target_group.add_mutually_exclusive_group()
+    sdopts.add_argument("--with-spi-sdcard",        action="store_true", help="Enable SPI-mode SDCard support.")
+    sdopts.add_argument("--with-sdcard",            action="store_true", help="Enable SDCard support.")
+    # viopts = parser.target_group.add_mutually_exclusive_group()
+    # viopts.add_argument("--with-video-terminal",    action="store_true", help="Enable Video Terminal (VGA).")
+    # viopts.add_argument("--with-video-framebuffer", action="store_true", help="Enable Video Framebuffer (VGA).")
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq           = args.sys_clk_freq,
+        with_ethernet          = args.with_ethernet,
+        eth_phy                = args.eth_phy,
+        with_etherbone         = args.with_etherbone,
+        eth_ip                 = args.eth_ip,
+        eth_dynamic_ip         = args.eth_dynamic_ip,
+
+        # with_video_terminal    = args.with_video_terminal,
+        # with_video_framebuffer = args.with_video_framebuffer,
+         with_spi_flash = args.with_spi_flash,
+        **parser.soc_argdict
+    )
+    if args.with_spi_sdcard:
+        soc.add_spi_sdcard()
+    if args.with_sdcard:
+        soc.add_sdcard()
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
